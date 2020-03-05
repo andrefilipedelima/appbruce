87ab0355894ff2efe10b9b41eb2b7dfb5dd06a43
thiago_acam <thiago.acam@gmail.com>
2020-03-04 22:07:07 -0300

    Voltando

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { ToastService } from 'src/app/services/toastService';
 
 @Component({
   selector: 'app-login',
@@ -8,19 +10,41 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  //loginForm: FormGroup;
+  toastService: ToastService;
 
+  constructor(private authService: AuthService, private route: ActivatedRoute, private _toastService: ToastService) { 
+    console.log(this.route.queryParams);
+//    this.route.queryParams.subscribe(params => {
+//      console.log(params);
+//      if (params && params.message) {
+//        this.toastService.presentToast(params.message);
+//      }
+//    });
 
-  constructor(private authService: AuthService) { }
+this.toastService = _toastService;
+
+}
 
   ngOnInit() {
-   
+
+    //this.loginForm = new FormGroup({
+    //  email: new FormControl('', {
+    //    validators: [Validators.required, Validators.email]
+    //  }),
+    //  password: new FormControl('', {validators: [Validators.required]})
+    //});
+
   }
 
-  onSubmit(form: NgForm){
-    this.authService.login({
+  async onSubmit(form: NgForm){
+    let login = await this.authService.login({
       email: form.value.email,
       password: form.value.password
     });
+
+    if(!login)
+      this.toastService.presentToast('Usuário ou senha inválidos');
   }
 
 
